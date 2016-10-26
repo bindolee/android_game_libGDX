a41d13d7a6ed2237269f0743b0fe44f6b88eae98
bindolee <bindolee@gmail.com>
2016-10-26 12:56:06 -0700

    first android/java game using libGDX

---
+++ desktop/src/com/simple/shooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.simple.shooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.simple.shooter.shooterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new shooterGame(), config);
+	}
+}
