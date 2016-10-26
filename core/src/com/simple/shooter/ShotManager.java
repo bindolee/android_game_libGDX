7f38561ce4af5ca06a49457b5830f25db4ab7474
bindolee <bindolee@gmail.com>
2016-10-26 16:11:38 -0700

    Added shot delay.. means..it shot every .5 seconds...

--- core/src/com/simple/shooter/ShotManager.java
+++ core/src/com/simple/shooter/ShotManager.java
@@ -1,5 +1,6 @@
 package com.simple.shooter;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,8 +16,11 @@ public class ShotManager {
 
     public static final int SHOT_Y_OFFSET = 90;
     public static final int SHOT_SPEED = 500;
+    public static final double MINMUM_TIME_BETWEEN_SHOTS = .5f; //every 5/10 seconds
+
     private final Texture shotTexture;
     private List<AnimatedSprite> shots = new ArrayList<AnimatedSprite>();
+    private float timeSinceLastShot = 0;
 
     public ShotManager(Texture shotTexture)
     {
@@ -31,17 +35,21 @@ public class ShotManager {
             newShotAnimated.setPosition(shipCenterXLocation, SHOT_Y_OFFSET);
             newShotAnimated.setVelocity(new Vector2(0, SHOT_SPEED));
             shots.add(newShotAnimated);
+            timeSinceLastShot = 0f;
         }
     }
 
     private boolean canFireShot() {
-        return true;
+        return timeSinceLastShot > MINMUM_TIME_BETWEEN_SHOTS;
     }
 
     public void update() {
         for (AnimatedSprite shot : shots){
             shot.move();
         }
+
+        // Increment by delta time 
+        timeSinceLastShot += Gdx.graphics.getDeltaTime();
     }
 
     public void draw(SpriteBatch batch) {
