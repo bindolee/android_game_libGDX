4cc43f14b5dd68e8836ee8fd8196d2a1bf5b3f68
bindolee <bindolee@gmail.com>
2016-10-26 13:17:22 -0700

    Migrated files from example

--- core/src/com/simple/shooter/shooterGame.java
+++ core/src/com/simple/shooter/shooterGame.java
@@ -1,33 +1,68 @@
 package com.simple.shooter;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class shooterGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import javax.microedition.khronos.opengles.GL10;
+
+public class shooterGame implements ApplicationListener {
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture texture;
+	private Sprite sprite;
+
 	@Override
-	public void create () {
+	public void create() {
+
+		//Texture.setEnforcePotImages(false);
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
+		camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		texture = new Texture(Gdx.files.internal("libgdx.png"));
+		texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+
+		sprite = new Sprite(region);
+		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	public void dispose() {
+		batch.dispose();
+		texture.dispose();
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
 	}
 }
